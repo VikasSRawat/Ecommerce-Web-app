1b36a0fd8e8dab5b93a91c8591bab3792c6eadcb
VikasSRawat <90858879+VikasSRawat@users.noreply.github.com>
2023-11-03 22:56:33 +0530

    Add files via upload

---
+++ pricePredictorAPI/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import ObjectId
+from pymongo import MongoClient
+from flask_cors import CORS
+import pythonScript
+
+# uri='mongodb+srv://karanmouryadmp:karan@cluster0.fzwllfy.mongodb.net/?retryWrites=true&w=majority'
+uri = 'mongodb+srv://VRGarages:mern1234@cluster0.oevbnx8.mongodb.net/veggieHub'
+client=MongoClient(uri,tls=True,tlsAllowInvalidCertificates=True)
+
+app=Flask(__name__)
+
+mydatabase=client['flaskcrud']
+db=mydatabase['users']
+
+CORS(app)
+@app.route('/',methods=['GET'])
+def get():
+    return jsonify({'msg':'Hello'})
+
+@app.route('/users',methods=['GET'])
+def getUsers():
+    users=[]
+    for doc in db.find():
+        users.append({
+            'state':doc['state'],
+            'season':doc['season'],
+            'name':doc['name'],
+            'availability':doc['availability'],
+            'demand':doc['demand'],
+            'output':doc['output']
+        })
+        return jsonify(users)
+
+@app.route('/users',methods=['Put'])
+def update():
+    data=pythonScript.predictPrice(request.json['name'],request.json['season'],request.json['state'],request.json['availability'],request.json['demand'])
+    db.update_one({'_id':ObjectId(request.json['id'])},{'$set':{
+        'name':request.json['name'],
+        'season':request.json['season'],
+        'state':request.json['state'],
+        'availability':request.json['availability'],
+        'demand':request.json['demand'],
+        'output':str(data)
+    }})
+    return jsonify({'msg':"updated"})
+
+if __name__=='__main__':
+    app.run(debug=True)
